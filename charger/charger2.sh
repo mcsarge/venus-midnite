1405d268952150af2b0826a90f79d9ee0712295c
Matt Sargent <matthew.c.sargent@gmail.com>
2026-07-19 15:46:19 -0400

    Change unit number from 11 to 10 in charger2.sh

--- charger/charger2.sh
+++ charger/charger2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 /usr/bin/python3 /data/classic/charger/charger.py \
-    --ip 192.168.0.226 --port 502 --unit 11 --device-instance 1
+    --ip 192.168.0.226 --port 502 --unit 10 --device-instance 1
